feaddce1658de4cb9decb410f7724b158d38cf82
Kou Kao <kou@torchtechpro.com>
2023-05-15 16:13:52 +0800

    加入vue + iview

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+    <Header></Header>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> |
+      <router-link to="/info">Info</router-link>
+    </div>
+    <router-view />
+  </div>
+</template>
+<script>
+import Header from "./views/Header.vue";
+export default {
+  components: {
+    Header,
+  },
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  background-color: #f9f9f9;
+  min-height: 100vh;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
---
+++ src/views/Header.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <Row class="header">
+      <Col :xs="12" :sm="12" :md="8" :lg="8">
+        <h2>{{ mainTitle }}</h2>
+      </Col>
+      <template v-for="(item, index) in headerList">
+        <Col :key="index">
+          <h3>{{ item.title }}</h3>
+        </Col>
+      </template>
+    </Row>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      mainTitle: "毛孩鮮食救星",
+      headerList: [
+        {
+          title: "近期活動",
+          path: "/activity",
+        },
+        {
+          title: "毛孩寫真",
+          path: "/activity",
+        },
+        {
+          title: "領養資訊",
+          path: "/activity",
+        },
+      ],
+    }
+  },
+  methods: {
+    toPage (path) {
+      this.$router.push({
+        path: path,
+      });
+    },
+  },
+}
+</script>
+<style lang="scss" scoped>
+.header {
+  background-color: #fff;
+  height: 64px;
+  box-shadow: 1px 2px 5px #eee;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+}
+</style>
